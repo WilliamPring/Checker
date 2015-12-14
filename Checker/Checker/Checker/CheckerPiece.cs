8ab1d453773d4e18a007c511b14d7dbb713fd9d8
Kid Putty <afg_naween@hotmail.com>
2015-12-13 17:31:51 -0800

    Here's the Board Layout and Pieces done in Xaml

--- Checker/Checker/Checker/CheckerPiece.cs
+++ Checker/Checker/Checker/CheckerPiece.cs
@@ -5,35 +5,69 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Shapes;
 
 namespace Checker
 {
     public class CheckerPiece
     {
         //true reprsent white and false represent black
-        private bool color;
+        private bool colorStatus;
+
+        private double height;
+        private double width;  
+
         private int xPos;
         private int yPos;
+
         private int CanvasPostY;
         private int CanvasPostX;
-        public CheckerPiece(int xPos, int yPos, int CanvasPostY, int CanvasPostX, bool color)
+
+        private SolidColorBrush checkerColor; 
+
+        /** Contstructor **/
+
+        public CheckerPiece(int xPos, int yPos, int CanvasPostY, int CanvasPostX, bool colorStatus, double height, double width)
         {
-            this.XPos = xPos;
-            this.YPos = yPos;
+            //Positions
+            this.xPos = CanvasPostX;
+            this.yPos = CanvasPostY;
             this.CanvasPostX = CanvasPostX;
             this.CanvasPostY = CanvasPostY;
-            this.color = color;
+
+            //Image related data members
+            this.colorStatus = colorStatus;
+            this.height = height;
+            this.width  = width;
+
+            //Establish the color of the checker piece
+            if(colorStatus == true) //Represents color Red
+            {
+                checkerColor = new SolidColorBrush(Colors.Red); 
+            }
+            else //Represents color Green
+            {
+                checkerColor = new SolidColorBrush(Colors.Green);
+            }
         }
+
+        //Default
+        public CheckerPiece()
+        {
+
+        }
+
+
+        /** Properties **/
+
         public int XPos
         {
             get
             {
                 return xPos;
-            }
-
-            set
-            {
-                xPos = value;
             }
         }
 
@@ -43,32 +77,31 @@ namespace Checker
             {
                 return yPos;
             }
-
-            set
-            {
-                yPos = value;
-            }
         }
-
-        public bool Color
+           
+        public double Height
         {
             get
             {
-                return color;
-            }
-
-            set
-            {
-                color = value;
+                return height; 
             }
         }
 
-        public CheckerPiece()
+        public double Width
         {
-
+            get
+            {
+                return width; 
+            }
         }
 
-
-
+        public SolidColorBrush CheckerColor
+        {
+            get
+            {
+                return checkerColor;
+            }
+        }
+        
     }
 }
--- Checker/Checker/Checker/MainPage.xaml.cs
+++ Checker/Checker/Checker/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,8 +13,10 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
 
 namespace Checker
 {
@@ -27,6 +30,11 @@ namespace Checker
         private int canvasXPos;
         private int canvasYPos;
 
+        private int totRows;
+        private int totColumns;
+
+        private CheckerPiece myCheckerPiece; 
+
         public MainPage()
         {
             this.InitializeComponent();
@@ -34,8 +42,14 @@ namespace Checker
             canvasYPos = 0;
             WhitePiece = new List<CheckerPiece>();
             BlackPiece = new List<CheckerPiece>();
-            
-        }
+
+
+
+
+        }         
+
+
+
 
         public void CheckMove(List<CheckerPiece> MovePiece, CheckerPiece CurrentPost, CheckerPiece DesirePost)
         {
@@ -65,32 +79,32 @@ namespace Checker
 
 
         public void CreateCheckerPiece()
-        {
-            //cannot get the size dynamicly so....
-
-            canvasXPos = (int)myCanvas.ActualWidth;
-            canvasYPos = (int)myCanvas.ActualHeight;
+        {        
             //loop through a grid for the y position
             for (int i = 1; i <= 8; i++)
             {
                 //loop through the gird for the x position
-                for (int w = 1; w <= 8; w++)
+                for (int w = 0; w < 8; w++)
                 {
                     //put a checker piece only if i is less then 3 because 3 represent the all the Black Piece
                     if (i <= 3)
                     {
                         if (i % 2 == 0)
                         {
-                            if (w % 2 == 0)
+                            if (w % 2 != 0)
                             {
-                                BlackPiece.Add(new CheckerPiece(i, w, canvasXPos * i, canvasYPos * w, false));
+                                myCheckerPiece = new CheckerPiece(i, w, canvasXPos * i, canvasYPos * w, false, canvasXPos, canvasXPos);
+                                BlackPiece.Add(myCheckerPiece);
+                                DrawCheckerPiece(myCheckerPiece, i, w);
                             }
                         }
                         else
                         {
-                            if (w % 2 != 0)
+                            if (w % 2 == 0)
                             {
-                                BlackPiece.Add(new CheckerPiece(i, w, canvasXPos * i, canvasYPos * w, false));
+                                myCheckerPiece = new CheckerPiece(i, w, canvasXPos * i, canvasYPos * w, false, canvasXPos, canvasXPos);
+                                BlackPiece.Add(myCheckerPiece);
+                                DrawCheckerPiece(myCheckerPiece, i, w);
                             }
                         }
                     }
@@ -98,26 +112,187 @@ namespace Checker
                     {
                         if (i % 2 == 0)
                         {
-                            if (w % 2 == 0)
+                            if (w % 2 != 0)
                             {
-                                WhitePiece.Add(new CheckerPiece(i, w, canvasXPos * i, canvasYPos * w, true));
+                                myCheckerPiece = new CheckerPiece(i, w, canvasXPos * i, canvasYPos * w, true, canvasXPos, canvasXPos);
+                                WhitePiece.Add(myCheckerPiece);
+                                DrawCheckerPiece(myCheckerPiece, i, w);
                             }
                         }
                         else
                         {
-                            if (w % 2 != 0)
+                            if (w % 2 == 0)
                             {
-                                WhitePiece.Add(new CheckerPiece(i, w, canvasXPos * i, canvasYPos * w, true));
+                                myCheckerPiece = new CheckerPiece(i, w, canvasXPos * i, canvasYPos * w, true, canvasXPos, canvasXPos);
+                                WhitePiece.Add(myCheckerPiece);
+                                DrawCheckerPiece(myCheckerPiece, i, w);
                             }
                         }
                     }
                 }
+            } 
+        }
+
+
+        private void DrawCheckerPiece(CheckerPiece myPiece, int rowNum, int colNum)
+        {
+            //Add canvas to the grid
+            Canvas myCanvas = new Canvas();
+            myGrid.Children.Add(myCanvas);
+
+            Ellipse newCheckerImage = new Ellipse();
+
+            newCheckerImage.Height = myPiece.Height;
+            newCheckerImage.Width = myPiece.Width;
+
+            newCheckerImage.Margin = new Thickness(0, 5, 0, 0); //Always the same distance apart
+            myCanvas.SetValue(Grid.ColumnProperty, colNum);
+            myCanvas.SetValue(Grid.RowProperty, rowNum);
+
+            newCheckerImage.Fill = myPiece.CheckerColor;
+            myCanvas.Children.Add(newCheckerImage); //Finally draw the ellipse onto the canvas
+        }
+
+
+        private void CreateBoard()
+        {
+            Rectangle squareOnBoard;
+            int row = 1;
+            int column = 0;
+
+            for (int i = 1; i <= 8; i++)
+            {
+                for (int j = 1; j <= 8; j++)
+                {
+                    if (i % 2 == 0) //Black square
+                    {
+                        if (j % 2 == 0)
+                        {
+                            squareOnBoard = new Rectangle();
+                            squareOnBoard.Fill = new SolidColorBrush(Colors.Black);
+                            squareOnBoard.SetValue(Grid.ColumnProperty, column);
+                            squareOnBoard.SetValue(Grid.RowProperty, row);
+                            myGrid.Children.Add(squareOnBoard);
+                        }
+
+
+                    }
+                    else //White Square
+                    {
+                        if (j % 2 != 0)
+                        {
+                            squareOnBoard = new Rectangle();
+                            squareOnBoard.Fill = new SolidColorBrush(Colors.Black);
+                            squareOnBoard.SetValue(Grid.ColumnProperty, column);
+                            squareOnBoard.SetValue(Grid.RowProperty, row);
+                            myGrid.Children.Add(squareOnBoard);
+                        }
+                    }
+                    column++;
+                }
+                row++;
+                column = 0;
+            }
+        }     
+
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {       
+            ////Break up the board into pieces
+            //totColumns = (int)myGrid.ActualWidth;
+            //totRows = (int)myGrid.ActualHeight;
+
+            //canvasXPos = totColumns / 8;
+            //canvasYPos = totRows / 8;
+
+            //CreateBoard(); //Create the board
+            //CreateCheckerPiece(); //Assign the pieces to the board 
+        }
+
+        private void Button_Clicked(object sender, RoutedEventArgs e)
+        {
+            myGrid.Background = new SolidColorBrush(Colors.Purple);
+
+
+
+
+        }
+
+        private void Clicked(object sender, TappedRoutedEventArgs e)
+        {
+            Rectangle squareOnBoard = sender as Rectangle;
+
+            int row = (int)squareOnBoard.GetValue(Grid.RowProperty);
+            int column = (int)squareOnBoard.GetValue(Grid.ColumnProperty);
+            bool blackSquareStatus = false;
+
+            if(row % 2 == 0) //Even Number
+            {
+                if(column % 2 != 0) //Ding ding Black Square
+                {
+                    blackSquareStatus = true; //Highlight the square 
+                }
+            }
+            else //Odd Number 
+            {
+                if(column % 2 == 0) //Ding ding Black Square
+                {
+                    blackSquareStatus = true; //Highlight the square
+                }
+            }
+
+
+            if(blackSquareStatus)
+            {
+                SolidColorBrush compareBrush = new SolidColorBrush(Colors.Yellow);
+                SolidColorBrush squareBrush = (SolidColorBrush)squareOnBoard.Fill;
+
+                if (squareBrush.Color == compareBrush.Color)
+                {
+                    squareOnBoard.Fill = new SolidColorBrush(Colors.Black);
+                }
+                else
+                {
+                    squareOnBoard.Fill = new SolidColorBrush(Colors.Yellow);                    
+                }
             }
         }
 
-        private void Loaded(object sender, RoutedEventArgs e)
+        //private void Play_Game_Clicked(object sender, RoutedEventArgs e)
+        //{
+        //    totColumns = (int)myGrid.ActualWidth;
+        //    totRows = (int)myGrid.ActualHeight;
+
+        //    canvasXPos = totColumns / 8;
+        //    canvasYPos = totRows / 8;
+
+        //    playButton.IsEnabled = false;
+        //    playButton.Opacity = 0;
+
+        //    CreateCheckerPiece();
+
+        //}
+
+        private void YOO(object sender, TappedRoutedEventArgs e)
         {
-            CreateCheckerPiece();
+            Canvas squareOnBoard = sender as Canvas;
+
+            //int row = (int)squareOnBoard.GetValue(Grid.RowProperty);
+            int column = (int)squareOnBoard.GetValue(Grid.ColumnProperty);
+            int row = (int)squareOnBoard.GetValue(Grid.RowProperty);
+            int i = 0;
+
+            Ellipse myRobot = new Ellipse();
+
+            myRobot.Height = 50;
+            myRobot.Width = 50;
+
+            myRobot.Fill = new SolidColorBrush(Colors.Green);
+
+            squareOnBoard.SetValue(Grid.ColumnProperty, 4);
+
+            squareOnBoard.Children.Add(myRobot);
+
         }
     }
 }
